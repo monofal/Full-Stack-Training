8698e188d1b18f1cc6d6559f52db03836a60b3f7
Nofal.Fayyaz <nofal@unassigned.arbisoft.com>
2019-09-04 18:28:19 +0500

    Unit Test Cases Added

---
+++ Test_Task2.py
@@ -0,0 +1,12 @@
+import Task2
+import unittest
+
+
+class MyTestCase(unittest.TestCase):
+    def test_multiply(self):
+        result = Task2.multiplyDigitsOfNumber(1234)
+        self.assertEqual(result, 24)
+
+
+if __name__ == '__main__':
+    unittest.main()
