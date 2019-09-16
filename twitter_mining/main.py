c3681f803b516f52458f3d3bb3a7c057bd96c822
Nofal.Fayyaz <nofal@unassigned.arbisoft.com>
2019-09-16 12:08:57 +0500

    api calls

--- twitter_mining/main.py
+++ twitter_mining/main.py
@@ -5,7 +5,7 @@ import argparse
 from twitter_mining.http.http_handler import HttpHandler
 
 
-def main(argv):
+def main():
     parser = argparse.ArgumentParser()
     parser.add_argument("search_query", help="Search Query")
     parser.add_argument("tweet_count", help="Tweet Count")
@@ -15,12 +15,21 @@ def main(argv):
     search_query = args.search_query
     tweet_count = args.tweet_count
 
-    host = "https://api.assurestor.com/api/infrascale/monitoring/historyevents/summary"
-    api_key = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1bmlxdWVfbmFtZSI6IjRkOTc1YTJhLWFjMDktNDkwNC0zZWY1LTA4ZDczMWYxMDA2OCIsIm5iZiI6MTU2ODA2MzM3NSwiZXhwIjoxNTY4NjY4MTc1LCJpYXQiOjE1NjgwNjMzNzV9.bTLX86BpZne-5nrNbHLKjwpzG_fIsUYL0PKeFznlrTw'  # os.environ.get('API_KEY')
+    host = "https://api.twitter.com/1.1/search/tweets.json?q=from%3Atwitterdev&result_type=mixed&count=2"
+    api_key = 'ria2kzrCoUlnMgrrDXWZmfeLeconsumerSecretKey=U7HPFUFQsgZSDaB1OJQ7Hx4vIasVJw9fqlQdIOEKXnd2TL3nLTaccessToken=1013682218101768192-WLU00n8Sh1Ku09I0rupZquybz0qIw2accessTokenSecret=Hra6GzKLYEIvTHGBAPGS733UJFOuPhydtgxzw8kPzhrR8'  # os.environ.get('API_KEY')
     request_headers = {
-        "Authorization": 'Bearer ' + format(api_key),
-        'Content-Type': 'application/json'
+        'authorization': 'OAuth oauth_consumer_key=' + format(api_key),
+        'oauth_nonce' : 'generated-nonce',
+        'oauth_signature' : 'generated-signature',
+        'oauth_signature_method' : 'HMAC-SHA1',
+        'oauth_timestamp' : 'generated-timestamp',
+        'oauth_token' : 'access-token-for-authed-user',
+        'oauth_version' : '1.0'
     }
+
+
+
+
     data = {
         "period": "last24h",
         "accountType": '1'
@@ -31,11 +40,11 @@ def main(argv):
                           version=None)
 
     # GET collection
-    response = handler.get(query_params=data)
+    response = handler.get()
     print(response.status_code)
     print(response.headers)
     print(response.body)
 
 
 if __name__ == "__main__":
-    main(sys.argv[1:])
+    main()
