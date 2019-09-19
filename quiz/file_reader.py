a48c29d1011aca2acfabd3841d1b9a0d39fdf510
Nofal.Fayyaz <nofal@unassigned.arbisoft.com>
2019-09-19 20:36:43 +0500

    adding previous tasks

---
+++ quiz/file_reader.py
@@ -0,0 +1,58 @@
+"""
+Module handle file operations
+"""
+import re
+
+from question import Question
+
+
+class FileReader:
+    """
+    Class to perform file operations
+    """
+    file_format = None
+
+    def __init__(self):
+        self.file_format = '^\d+[\.]\s.*';
+
+    @staticmethod
+    def read_file(file_path):
+        """
+        Read keys from file and return list of keys
+        """
+        file_content = None
+        try:
+            with open(file_path) as f:
+                file_content = f.read().splitlines()
+        except FileNotFoundError:
+            raise
+
+        return file_content
+
+    def get_questions(self):
+        """
+        Read questions from file and return list of questions
+        """
+        questions = []
+        try:
+            question_content = self.read_file("questions.txt")
+            key_content = self.read_file("key.txt")
+
+            for question, key in zip(question_content, key_content):
+                if self.validate_format(question) and self.validate_format(key):
+                    # Read first character as id and from fourth character onward as text
+                    questions.append(Question(question[:1], question[3:], key[3:]))
+                else:
+                    raise Exception('File format is not correct , Expected Integer+Dot+Space+Text')
+
+        except FileNotFoundError:
+            print("File not found")
+
+        return questions
+
+    def validate_format(self,
+                        text):
+        """
+        Validate if text matches the pattern
+        """
+        return re.match(self.file_format, text)
