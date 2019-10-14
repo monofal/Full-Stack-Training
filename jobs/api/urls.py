c9235c28be69c25f3085765875a3416d958aee42
Nofal.Fayyaz <nofal@unassigned.arbisoft.com>
2019-10-14 12:30:31 +0500

    api_jwt_authentication_added

--- jobs/api/urls.py
+++ jobs/api/urls.py
@@ -1,5 +1,6 @@
 from django.urls import path
 from rest_framework.routers import DefaultRouter
+from rest_framework_simplejwt import views as jwt_views
 
 from .views import *
 
@@ -7,6 +8,8 @@ router = DefaultRouter()
 router.register('jobs', JobViewSet, base_name='jobs')
 
 urlpatterns = [
+    path('token/', jwt_views.TokenObtainPairView.as_view(), name='token_obtain_pair'),
+    path('token/refresh/', jwt_views.TokenRefreshView.as_view(), name='token_refresh'),
     path('search/', SearchApiView.as_view()),
     path('job/<int:id>', GetByJobIdView.as_view()),
     path('employer/applications/', EmployerApplicationsView.as_view()),
--- jobs/api/views.py
+++ jobs/api/views.py
@@ -1,7 +1,9 @@
 from rest_framework import viewsets
+from rest_framework.decorators import permission_classes
+from rest_framework.permissions import IsAuthenticated
 
 from .serializers import *
-from rest_framework.generics import ListAPIView, CreateAPIView
+from rest_framework.generics import ListAPIView
 
 
 class JobViewSet(viewsets.ReadOnlyModelViewSet):
@@ -9,6 +11,7 @@ class JobViewSet(viewsets.ReadOnlyModelViewSet):
     queryset = serializer_class.Meta.model.objects.all()
 
 
+@permission_classes((IsAuthenticated,))
 class SearchApiView(ListAPIView):
     """
     Return list of jobs that matches search parameters
@@ -22,6 +25,7 @@ class SearchApiView(ListAPIView):
             position__icontains=self.request.query_params.get('position', '')).order_by("-entry_timestamp")
 
 
+@permission_classes((IsAuthenticated,))
 class GetByJobIdView(ListAPIView):
     """
     Provide job by job id
@@ -36,19 +40,21 @@ class GetByJobIdView(ListAPIView):
         return queryset
 
 
+@permission_classes((IsAuthenticated,))
 class EmployerApplicationsView(ListAPIView):
     """
     List of applications applied against jobs posted by specific user
     """
-    serializer_class = ApplicantSerializer
-
     def get_queryset(self):
-        employer_id = self.request.query_params.get('employer_id')
+        employer_id = self.request.user.id
         if employer_id:
             return self.serializer_class.Meta.model.objects.filter(job__user_id=employer_id). \
                 order_by('-applied_timestamp')
 
+    serializer_class = ApplicantSerializer
 
+
+@permission_classes((IsAuthenticated,))
 class EmployerJobsView(ListAPIView):
     """
     Provider list of jobs posted by employer
@@ -56,7 +62,7 @@ class EmployerJobsView(ListAPIView):
     serializer_class = JobSerializer
 
     def get_queryset(self):
-        employer_id = self.request.query_params.get('employer_id')
+        employer_id = self.request.user.id
         if employer_id:
             return self.serializer_class.Meta.model.objects.filter(user_id=employer_id). \
                 order_by('-entry_timestamp')
